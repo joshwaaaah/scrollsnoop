1853e2c467e82fdad93bca2c80c066ae2b985d5d
Josh <jnhartshorne@gmail.com>
2025-07-26 17:36:46 +0100

    Generate types and allow publish to NPM

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom'
-  },
+  plugins: [dts({
+    outDir: 'dist',
+    rollupTypes: true
+  })],
+  build: {
+    lib: {
+      entry: 'lib/scrollsnoop.ts',
+      formats: ['es'],
+      name: 'ScrollSnoop',
+      fileName: 'scrollsnoop',
+    },
+  }
 })
